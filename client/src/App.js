4a9912c42c91fac844cf24cf9e903e298e542c42
Sumit Tuladhar <sumittuladhar@gmail.com>
2019-07-11 08:55:36 +0545

    implemented BookList using apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
 import BookList from './components/BookList';
 
-function App() {
-  return (
-    <div id="main">
-      <h1>Ninja's Reading List</h1>
-      <BookList />
-    </div>
-  );
+//apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
+export class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List - Sumit</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
+    )
+  }
 }
 
-export default App;
+export default App
--- server/app.js
+++ server/app.js
@@ -4,8 +4,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true
