301c54b5cda4745d7709453cfac9feb69bd34c2d
Sumit Tuladhar <sumittuladhar@gmail.com>
2019-07-11 09:14:01 +0545

    externalize the queries

---
+++ client/src/queries/queries.js
@@ -0,0 +1,23 @@
+import { gql } from 'apollo-boost';
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`; 
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`; 
+
+export {
+  getAuthorsQuery, getBooksQuery
+};
